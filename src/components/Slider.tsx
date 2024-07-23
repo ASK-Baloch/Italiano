1de8bb0fc7e6f4399524fe6e29a44fd8b0272b3e
Ahmed <balochsaeed888@gmail.com>
2024-07-23 13:54:32 +0500

    slider section completed

--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -1,39 +1,55 @@
 "use client";
 import Image from "next/image";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 const data = [
   {
-    id:1,
-    title:"always fresh & delicious crispy & spicy hot",
-    image:"/slide1.png"
+    id: 1,
+    title: "always fresh & delicious crispy & spicy hot",
+    image: "/slide1.png",
   },
   {
-    id:2,
-    title:"we deliver your order in 30 minutes",
-    image:"/slide2.png"
+    id: 2,
+    title: "we deliver your order in 30 minutes",
+    image: "/slide4.webp",
   },
   {
-    id:3,
-    title:"the best italian pizza in town",
-    image:"/slide3.png"
-  }
-]
+    id: 3,
+    title: "the best italian pizza in town",
+    image: "/slide3.jpg",
+  },
+];
 
 const Slider = () => {
   const [slide, setSlide] = useState(0);
+
+  // useEffect(() => {
+  //   const interval = setInterval(
+  //     () => setSlide((prev) => (prev === data.length - 1 ? 0 : prev + 1)),
+  //     2000
+  //   );
+  //   return () => clearInterval(interval);
+  // }, []);
+
   return (
-    <div className="flex flex-col h-[calc(100vh-6rem)] md:h-[calc(100vh-9rem)]">
+    <div className="flex flex-col h-[calc(100vh-6rem)] md:h-[calc(100vh-9rem)] lg:flex-row bg-fuchsia-50">
       {/*  TEXT CONTAINER */}
-      <div className="text-[#B40614] h-1/2 flex items-center justify-center flex-col gap-8 font-bold">
-        <h1 className="text-5xl text-center uppercase p-4 md:text-6xl xl:text-7xl">
-          Slider
+      <div className="text-[#B40614] flex-1 flex items-center justify-center flex-col gap-8 font-bold ">
+        <h1 className="text-5xl text-center uppercase p-4 md:p-10 md:text-6xl xl:text-7xl">
+          {data[slide].title}
         </h1>
-        <button className="bg-[#B40614] text-white py-4 px-8 rounded">Order now</button>
+        <button className="bg-[#B40614] text-white py-4 px-8 rounded">
+          Order now
+        </button>
       </div>
       {/*  IMAGE CONTAINER */}
-      <div className="h-1/2 relative w-full">
-        <Image src="/slide1.png" alt="slider" fill className="object-cover" />
+      <div className="w-full relative flex-1">
+        <Image
+          src={data[slide].image}
+          alt="slider"
+          fill
+          className="object-cover"
+        />
       </div>
     </div>
   );
