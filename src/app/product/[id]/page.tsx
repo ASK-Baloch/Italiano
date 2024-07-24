9da46a34215ebc66d57dcc23d2ffa9d02d4fd94f
Ahmed <balochsaeed888@gmail.com>
2024-07-24 17:57:37 +0500

    now working on product/id or single product page

--- src/app/product/[id]/page.tsx
+++ src/app/product/[id]/page.tsx
@@ -1,8 +1,13 @@
+import Image from 'next/image'
 import React from 'react'
 
 const SingleProductPage   = () => {
   return (
-    <div>SingleProductPage  </div>
+    <div className='p-4 lg:px-20 xl:px-40 h-screen flex flex-col justify-around text-[#B40614] md:flex-row'>
+      <div className="relative">
+        <Image />
+      </div>
+    </div>
   )
 }
 
