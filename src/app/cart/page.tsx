4d0d9b2079bcf3510453e2f64adb06f4247bd0ec
Ahmed <balochsaeed888@gmail.com>
2024-07-26 20:37:16 +0500

    cart page done

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -1,33 +1,67 @@
-import Image from 'next/image'
-import React from 'react'
+import Image from "next/image";
+import React from "react";
 
 const CartPage = () => {
   return (
-    <div className='h-[calc(100vh-6rem)] md:h-[calc(100vh-9rem)]'>
+    <div className="h-[calc(100vh-6rem)] md:h-[calc(100vh-9rem)] flex flex-col text-[#b40614] lg:flex-row">
       {/* PRODUCT CONTAINER */}
-      <div className=''>
-         {/* SINGLE TEAM */}
-         <div className=''>
-            <Image src='/temporary/p1.png' alt='' width={100} height={100}/>
-            <div className=''>
-              <h1>sicilian</h1>
-              <span>Large</span>
-            </div>
-            <h2>$79.98</h2>
-            <span>X</span>
-         </div>
+      <div className="h-1/2 p-4 flex flex-col justify-center overflow-scroll lg:h-full lg:w-2/3 2xl:w-1/2 lg:px-20 xl:px-40">
+        {/* SINGLE TEAM */}
+        <div className="flex items-center justify-between mb-4 ">
+          <Image src="/temporary/p1.png" alt="" width={100} height={100} />
+          <div className="">
+            <h1 className="uppercase text-xl font-bold">sicilian</h1>
+            <span>Large</span>
+          </div>
+          <h2 className="font-bold">$79.98</h2>
+          <span className="cursor-pointer">X</span>
+        </div>
+        {/* SINGLE TEAM */}
+        <div className="flex items-center justify-between mb-4 ">
+          <Image src="/temporary/p1.png" alt="" width={100} height={100} />
+          <div className="">
+            <h1 className="uppercase text-xl font-bold">sicilian</h1>
+            <span>Large</span>
+          </div>
+          <h2 className="font-bold">$79.98</h2>
+          <span className="cursor-pointer">X</span>
+        </div>
+        {/* SINGLE TEAM */}
+        <div className="flex items-center justify-between mb-4 ">
+          <Image src="/temporary/p1.png" alt="" width={100} height={100} />
+          <div className="">
+            <h1 className="uppercase text-xl font-bold">sicilian</h1>
+            <span>Large</span>
+          </div>
+          <h2 className="font-bold">$79.98</h2>
+          <span className="cursor-pointer">X</span>
+        </div>
       </div>
       {/* PAYMENT CONTAINER */}
-      <div className=''>  
-        <div className=''>
-          <span className=''>Subtotal (3 items)</span>
-          <span className=''>$81.70</span>
+      <div className="h-1/2 p-4 bg-fuchsia-50 flex flex-col gap-4 justify-center lg:h-full lg:w-1/3 2xl:w-1/2 lg:px-20 xl:px-40 2xl:text-xl 2xl:gap-6">
+        <div className="flex justify-between">
+          <span className="">Subtotal (3 items)</span>
+          <span className="">$81.70</span>
         </div>
-        <hr  className=''/>
-        <button className='bg-[#B40614] text-[#CFB54F] font-bold p-2 rounded-md uppercase w-1/2'>Checkout</button>
+        <div className="flex justify-between">
+          <span className="">Service Cost</span>
+          <span className="">$0.00</span>
+        </div>
+        <div className="flex justify-between">
+          <span className="">Delivery Charges</span>
+          <span className="text-[#34912A]">FREE!</span>
+        </div>
+        <hr className="my-2" />
+        <div className="flex justify-between">
+          <span className="">TOTAL(INCL.VAT)</span>
+          <span className="text-[#2F3234] font-bold">$81.70</span>
+        </div>
+        <button className="bg-[#B40614] text-[#CFB54F] font-bold p-2 rounded-md uppercase w-1/2 self-end">
+          Checkout
+        </button>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default CartPage
+export default CartPage;
