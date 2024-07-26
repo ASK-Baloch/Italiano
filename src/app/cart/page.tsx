ffc9cc9b7e39e3310c11b383528bad2b0076cb9d
Ahmed <balochsaeed888@gmail.com>
2024-07-26 20:15:19 +0500

    working on cart page

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -1,8 +1,32 @@
+import Image from 'next/image'
 import React from 'react'
 
 const CartPage = () => {
   return (
-    <div>CartPage</div>
+    <div className='h-[calc(100vh-6rem)] md:h-[calc(100vh-9rem)]'>
+      {/* PRODUCT CONTAINER */}
+      <div className=''>
+         {/* SINGLE TEAM */}
+         <div className=''>
+            <Image src='/temporary/p1.png' alt='' width={100} height={100}/>
+            <div className=''>
+              <h1>sicilian</h1>
+              <span>Large</span>
+            </div>
+            <h2>$79.98</h2>
+            <span>X</span>
+         </div>
+      </div>
+      {/* PAYMENT CONTAINER */}
+      <div className=''>  
+        <div className=''>
+          <span className=''>Subtotal (3 items)</span>
+          <span className=''>$81.70</span>
+        </div>
+        <hr  className=''/>
+        <button className='bg-[#B40614] text-[#CFB54F] font-bold p-2 rounded-md uppercase w-1/2'>Checkout</button>
+      </div>
+    </div>
   )
 }
 
