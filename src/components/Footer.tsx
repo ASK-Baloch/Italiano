e6224bdb90d8181a37e983471f8239efedb4ee55
Ahmed <balochsaeed888@gmail.com>
2024-07-28 22:36:02 +0500

    couldnt solve the problem . the alignment is issue is just between 1000px to 1100px screensize . also footers padding mistake made right

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 const Footer = () => {
   const currentYear = new Date().getFullYear();
   return (
-    <div className='h-12 md:h-24 p-4 lg:p-20 xl:p-40 text-[#B40614] flex items-center justify-between'>
+    <div className='h-12 md:h-24 p-4 lg:px-20 xl:px-40 text-[#B40614] flex items-center justify-between'>
       <Link href='/' className='font-bold text-xl'>ITALIANO</Link>
       <span>&copy; {currentYear} ALL RIGHTS RESERVED</span>
     </div>
